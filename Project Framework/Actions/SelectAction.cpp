0706fa7fa102045a52987210eac0c138e78b943e
Seif <s-seif.raslan@zewailcity.edu.eg>
2022-05-22 23:53:55 +0200

    Type of selected figure shown

--- Project Framework/Actions/SelectAction.cpp
+++ Project Framework/Actions/SelectAction.cpp
@@ -1,8 +1,10 @@
 #include"SelectAction.h"
 #include "..\Figures\CFigure.h"
 #include "..\ApplicationManager.h"
-
-
+#include "..\Figures\CCircle.h"
+#include "..\Figures\CLine.h"
+#include "..\Figures\CRectangle.h"
+#include "..\Figures\CTriangle.h"
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
 
@@ -10,6 +12,11 @@ SelectAction::SelectAction(ApplicationManager* pApp) :Action(pApp)
 {
 }
 
+template<typename Base, typename T>
+inline bool instanceof(const T* ptr)
+{
+	return dynamic_cast<const Base*>(ptr) != nullptr;
+}
 
 SelectAction::~SelectAction(void)
 {
@@ -31,12 +38,27 @@ void SelectAction::ReadActionParameters() {
 		// If there is a figures inside the drawing area, proceed
 		if (FigureList[i] != nullptr) {
 
-			if (FigureList[i]->isWithinMe(click)) {
-				
-				pOut->PrintMessage("Figure: Selected");
+			if (FigureList[i]->isWithinMe(click))
+			{
+				if (instanceof<CCircle>(FigureList[i]))
+				{
+					pOut->PrintMessage("Circle selected");
+				}
+				else if (instanceof<CTriangle>(FigureList[i]))
+				{
+					pOut->PrintMessage("Triangle selected");
+				}
+				else if (instanceof<CRectangle>(FigureList[i]))
+				{
+					pOut->PrintMessage("Rectangle selected");
+				}
+				else if (instanceof<CLine>(FigureList[i]))
+				{
+					pOut->PrintMessage("Line selected");
+				}
 			}
 			else {
-				pOut->PrintMessage("Figure: Selecteddvfkdfvkpf");
+				pOut->PrintMessage("Please select a figure");
 			}
 		}
 	}
